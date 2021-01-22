74d102d8e1c1296a613b4261b6d690cefbd32a7b
Peter <31599233+peterjgithub@users.noreply.github.com>
2021-01-22 23:19:26 +0100

    stockexchanges added

--- app.py
+++ app.py
@@ -8,18 +8,42 @@ load_dotenv()
 app = Flask(__name__)
 today = date.today()    
 _tdate = str(today)
-url = 'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/minute/2020-11-01/"' + \
-    _tdate+'"?apiKey=HBSH9Fc30LZbumAvQXssFHbqw8jBgoeEcI3nTe'
-_true_url = url.replace('"', "")
-with urlopen(_true_url) as response:
-    source = response.read()
-data = json.loads(source)
-# print(source)
 
+
+def get_url_content(url):
+    _true_url = url.replace('"', "")
+    with urlopen(_true_url) as response:
+        source = response.read()
+    data = json.loads(source)
+    return data
+
+
+# url = 'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/minute/2020-11-01/"' + \
+#     _tdate + '"?apiKey=HBSH9Fc30LZbumAvQXssFHbqw8jBgoeEcI3nTe'
+# _true_url = url.replace('"', "")
+# with urlopen(_true_url) as response:
+#     source = response.read()
+# data = json.loads(source)
+# # print(source)
+
+# url = 'https://api.polygon.io/v1/meta/exchanges?apiKey=0kJfuiJORnO7nGk_Sh2EUiOJWeKpT04p'
+# _true_url = url.replace('"', "")
+# with urlopen(_true_url) as response:
+#     source = response.read()
+# data = json.loads(source)
 
 @app.route("/")
-def function():
-    return render_template("home.html", source_to_html_from_app=source)
+def home():
+    url = 'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/minute/2020-11-01/"' + _tdate + '"?apiKey=HBSH9Fc30LZbumAvQXssFHbqw8jBgoeEcI3nTe'
+    content = get_url_content(url)
+    return render_template("home.html", source_to_html_from_app=content)
+
+
+@app.route("/exchanges")
+def exchanges():
+    url = 'https://api.polygon.io/v1/meta/exchanges?apiKey=0kJfuiJORnO7nGk_Sh2EUiOJWeKpT04p'
+    content = get_url_content(url)
+    return render_template("stockexchanges.html", source_to_html_from_app=content)
 
 
 if __name__ == "__main__":
