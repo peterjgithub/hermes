0d0c80f18b6f7d54c3bf314c262bf278b1bcc08d
Peter <31599233+peterjgithub@users.noreply.github.com>
2021-01-22 22:25:48 +0100

    Created start.zsh + generated requirements.txt

---
+++ start.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+source venv/bin/activate
+flask run
+
+
+
+
+
+# to create a script:
+
+# 1) create new file 
+# eg start.zsh
+
+# 2) script must be an executable file
+# chmod +x start.zsh
+# chmod 755 start.zsh
+
+# 3) must start with a shebang line:
+# #!/usr/bin/env zsh
+
+# run:
+# ./start.zsh
