3580611a7c892ae8cedb249f9e56070ad35a5b71
Peter <31599233+peterjgithub@users.noreply.github.com>
2021-01-22 02:36:01 +0100

    added: basic .env and bootstrap

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template
-import json
 from urllib.request import urlopen
-import datetime
 from datetime import date
+import json
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-
-
-today = date.today()
+today = date.today()    
 _tdate = str(today)
-url = 'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/minute/2020-11-01/"'+_tdate+'"?apiKey=HBSH9Fc30LZbumAvQXssFHbqw8jBgoeEcI3nTe' 
+url = 'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/minute/2020-11-01/"' + \
+    _tdate+'"?apiKey=HBSH9Fc30LZbumAvQXssFHbqw8jBgoeEcI3nTe'
 _true_url = url.replace('"', "")
-with urlopen(_true_url) as response: 
-	source = response.read()
+with urlopen(_true_url) as response:
+    source = response.read()
 data = json.loads(source)
-print(source)
+# print(source)
+
 
 @app.route("/")
 def function():
-	return render_template("home.html",source_to_html_from_app = source)
-if __name__ == "__main__": 
-	app.run()
+    return render_template("home.html", source_to_html_from_app=source)
+
+
+if __name__ == "__main__":
+    app.run()
