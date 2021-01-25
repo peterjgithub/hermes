188a98bc48f4860c8965596d67f6fc8dcc34e109
Peter <31599233+peterjgithub@users.noreply.github.com>
2021-01-25 18:51:37 +0100

    organised in /connect subfolder (=middleware)

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ from datetime import date, datetime
 import json
 from dotenv import load_dotenv
 import os
-import requests
-from requests.exceptions import HTTPError, Timeout
-# BDay is business day, not birthday...
-from pandas.tseries.offsets import BDay
-from pandas import pandas as pd
+from connect.polygon import get_Grouped_Daily_Bars
+# from pandas import pandas as pd
 
 load_dotenv()
 
@@ -31,42 +28,6 @@ def get_url_content(url: str):
     jsondata = json.loads(source)
     return jsondata
 
-
-def __get_Grouped_Daily_Bars__(from_date: date):
-    _beginning_date = str(from_date)
-    print(f"__get_Grouped_Daily_Bars__(from_date={_beginning_date})")
-
-    try:
-        polygon_url = "https://api.polygon.io"
-        api = "/v2/aggs/grouped/locale/us/market/stocks/" + _beginning_date
-        payload = {
-            'unadjusted': "false",
-            'apiKey': '0kJfuiJORnO7nGk_Sh2EUiOJWeKpT04p',
-            }
-        timeout = 10
-        r = requests.get(polygon_url+api, params=payload, timeout=timeout)
-        rjson = r.json()
-        # resultlist = list(rjson["results"])
-        return rjson
-
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
-
-    except Timeout as timeout_err:
-        print(f'Timeout error occurred: {timeout_err}')
-
-    except Exception as err:
-        print(f'Other error occurred: {err}')
-
-
-# Get the daily open, high, low, and close (OHLC) for the entire stocks/equities markets.
-def get_Grouped_Daily_Bars(from_date: date):
-    last_business_day = date.today() - BDay(1)
-    from_date = from_date if from_date is not None else last_business_day
-    jsonresponse = __get_Grouped_Daily_Bars__(from_date)
-    return jsonresponse
-
-
 @app.route("/updates")
 def updates():
     jsontxt = get_Grouped_Daily_Bars(date(2021,1,21))
@@ -75,7 +36,6 @@ def updates():
     for quote in source:
         quote['t'] = pd.to_datetime(quote['t'], infer_datetime_format=True)
     return render_template("updates.html", source=source)
-
 
 @app.route("/")
 def home():
