47e4f6c246145092c9be5f2f33bf9f1c8980c58c
Peter <31599233+peterjgithub@users.noreply.github.com>
2021-01-26 00:02:42 +0100

    import & implement polygon-api-client

--- app.py
+++ app.py
@@ -2,64 +2,55 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from urllib.request import urlopen
 from datetime import date, datetime
-import json
+from polygon import RESTClient 
 from dotenv import load_dotenv
+from models import Quote
 import os
-from connect.polygon import get_Grouped_Daily_Bars
-# from pandas import pandas as pd
 
 load_dotenv()
+apiKey = os.getenv("POLYGON_APIKEY")
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 
-from models import Quote
 
-today = date.today()    
-_tdate = str(today)
+def ts_to_datetime(ts) -> str:
+    return datetime.fromtimestamp(ts / 1000.0).strftime('%Y-%m-%d %H:%M')
 
 
-def get_url_content(url: str):
-    _true_url = url.replace('"', "")
-    with urlopen(_true_url) as response:
-        source = response.read()
-    jsondata = json.loads(source)
-    return jsondata
+def ts_to_date(ts) -> str:
+    return date.fromtimestamp(ts / 1000.0).strftime('%Y-%m-%d')
 
-@app.route("/updates")
-def updates():
-    jsontxt = get_Grouped_Daily_Bars(date(2021,1,21))
-    source = jsontxt["results"]
-    print("look here!!!!")
-    for quote in source:
-        quote['t'] = pd.to_datetime(quote['t'], infer_datetime_format=True)
-    return render_template("updates.html", source=source)
 
 @app.route("/")
 def home():
-    apiKey = os.getenv("POLYGON_APIKEY")
-    url_core = "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-11-01/"
-    url = f"{url_core}{_tdate}?apiKey={apiKey}"    
-    content = get_url_content(url)
-    aapl_data = content["results"]
-    return render_template("home.html", source=aapl_data)
+    with RESTClient(apiKey) as client:
+        resp = client.stocks_equities_aggregates('AAPL', 1, 'day', date(2020,11,1), date.today())
+        quotes = resp.results
+        for quote in quotes:
+            quote['t'] = ts_to_date(quote['t'])
+    return render_template("home.html", source=quotes)
 
 
-@app.route("/exchanges")
-def exchanges():
-    url = 'https://api.polygon.io/v1/meta/exchanges?apiKey=0kJfuiJORnO7nGk_Sh2EUiOJWeKpT04p'
-    content = get_url_content(url)
-    return render_template("stockexchanges.html", source_to_html_from_app=content)
+@app.route("/updates")
+def updates():
+    with RESTClient(apiKey) as client:
+        resp = client.stocks_equities_grouped_daily('us', 'stocks', date(2021,1,22))
+        quotes = resp.results
+        for quote in quotes:
+            quote['t'] = ts_to_date(quote['t'])
+    return render_template("updates.html", source=quotes)
 
 
 @app.route("/tickers")
 def tickers():
-    url = 'https://api.polygon.io/v2/reference/tickers?sort=ticker&type=cs&perpage=20&page=1&apiKey=0kJfuiJORnO7nGk_Sh2EUiOJWeKpT04p'
-    content = get_url_content(url)
-    mytickers = content["tickers"]
+    with RESTClient(apiKey) as client:
+        resp = client.reference_tickers(sort='ticker')
+        mytickers = resp.tickers
     return render_template("tickers.html", source=mytickers)
+
 
 if __name__ == "__main__":
     app.run()
